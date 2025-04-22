f3d331ddf3942ffcf6ce72da281827d2cfa851aa
Peter Kabamba <kabambapeter3@gmail.com>
2025-04-22 14:53:28 +0200

    added the player ship to the game display.

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
-"""A class to store all settings for Alien Invasion."""
+class Settings:
+    """A class to store all settings for Alien Invasion."""
 
-def __init__(self):
-    """Initialize the gamse's settings."""
-    # Screen settings
-    self.screen_width = 1200
-    self.screen_height = 800
-    self.bg_color = (230, 230, 230)
+    def __init__(self):
+        """Initialize the gamse's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
